bf3346a2177cc68506d7e053909b7d9da1ed6710
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2022-08-04 10:09:53 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,76 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const buttonsArray = [
+  {
+    message: "apple",
+    emoji: "🍏"
+  },
+  {
+    message: "pineapple",
+    emoji: "🍍"
+  },
+  {
+    message: "kiwi",
+    emoji: "🥝"
+  },
+  {
+    message: "avokado",
+    emoji: "🥑"
+  },
+  {
+    message: "cherry",
+    emoji: "🍒"
+  },
+  {
+    message: "strawberry",
+    emoji: "🍓"
+  }
+];
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+class Button extends React.Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+    this.state = {timerValue : 0};
+    this.timerId = undefined;
+    this.title = this.getButtonTitle();
+    this.resetInterval = this.resetInterval.bind(this);
+  }
+
+  render() {
+    return (
+      <button onClick={this.resetInterval}>{this.title}</button>
+    )
+  }
+
+  resetInterval() {
+    
+    if (this.timerId !== undefined) {
+        clearInterval(this.timerId);
+        this.setState({timerValue: 0});
+        this.title = this.getButtonTitle();
+    }
+    this.timerId = setInterval(() => {
+        this.setState({timerValue: this.state.timerValue + 1});
+        this.title = this.getButtonTitle();
+    }, 1000);
+
+  }
+
+  getButtonTitle() {
+    return `eaten ${this.state.timerValue} ${this.props.message} ${this.props.emoji} `;
+}
+}
+
+root.render(
+  <>
+      {buttonsArray.map((e, index) => (
+        <Button {...e} />
+      ))}
+  </>
+)
